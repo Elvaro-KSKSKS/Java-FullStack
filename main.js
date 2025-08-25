b2987a392591aaf72eb2481ae4eabeb203f5872c
Elvaro-KSKSKS <100244045+Elvaro-KSKSKS@users.noreply.github.com>
2025-08-25 11:42:36 -0300

    Correção do formulário
    - Não é mais possível adicionar tarefas sem nome.

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 $(document).ready(function() {
     $('form').on('submit', function(e){
         e.preventDefault();
-    });
-
-    $('ul').on('click', 'li', function() {
-        $(this).toggleClass('done');
-    });
-
-    $('button').click(function(){
         const taskName = $('input').val();
         const newTask = $(`<li>${taskName}</li>`);
         $(newTask).appendTo('ul');
         $('input').val('');
+    });
+
+    $('ul').on('click', 'li', function() {
+        $(this).toggleClass('done');
     });
 });

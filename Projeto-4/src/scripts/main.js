b74bc635f41aebd50592569098ff26eaead9469e
Elvaro-KSKSKS <100244045+Elvaro-KSKSKS@users.noreply.github.com>
2025-12-26 18:04:38 -0300

    projeto-4

---
+++ Projeto-4/src/scripts/main.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const heroSection = document.querySelector('.hero');
+    const heroHeight = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const currentPosition = window.scrollY;
+
+        if (currentPosition < heroHeight) {
+            hideHeaderElements();
+        } else {
+            showHeaderElements();
+        };
+    });
+});
+
+function hideHeaderElements() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+};
+
+function showHeaderElements() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+};
+
